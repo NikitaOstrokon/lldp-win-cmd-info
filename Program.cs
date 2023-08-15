ce63b02875818971fcd4c9cd6bb537302e2e0d93
Nikita Ostrokon <142334357+NikitaOstrokon@users.noreply.github.com>
2023-08-15 19:42:50 +0300

    lldp-capture

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using SharpPcap;
 using PacketDotNet.LLDP;
 using System.Net;
+using System.IO;
+using System.Diagnostics;
 
 
 
@@ -36,9 +38,6 @@ namespace lldp
             string ver = SharpPcap.Version.VersionString;
             if (opt_json != true)
             {
-                Console.WriteLine("LLDPspy build {0} using lib v{1} 2017 Marco Götze, planetlan\n", build, ver);
-                Console.WriteLine("\n\n\toptional parameters:\n");
-                Console.WriteLine("\t/j\tOutput as JSON string\n\t/q\tQuit after first TLV received\n\n\tBest Usage for reuse of information lldp /j /q\n\n");
             }
             //if (opt_json == true) Console.WriteLine("JSON");
             //if (opt_quit == true) Console.WriteLine("QUIT");
@@ -76,6 +75,7 @@ namespace lldp
                         x++;
                     }
                     i++;
+
                 }
                 if (x < 1)
                 {
@@ -128,8 +128,10 @@ namespace lldp
                 device.Open(DeviceMode.Promiscuous, readTimeoutMilliseconds);
 
                 // tcpdump filter to capture only TCP/IP packets for LLDP and CDP
-                string filter = "ether[12:2]=0x88cc or ether[20:2]=0x2000";
+                //string filter = "ether[12:2]=0x88cc or ether[20:2]=0x2000";
+                string filter = "ether[12:2]=0x88cc";
                 device.Filter = filter;
+                Console.WriteLine(device.Name);
                 if (opt_json != true)
                 {
                     Console.WriteLine();
@@ -149,10 +151,138 @@ namespace lldp
                 if (opt_json == true) Console.WriteLine("{\"error\":\""+e.ToString()+"\",\"number\": 999}");
                 else
                 {
-                    Console.WriteLine("WinPCAP library is missing, please install from:");
-                    Console.WriteLine("\twin10x64:\twww.win10pcap.org\n");
-                    Console.WriteLine("\tother:\thttps://www.winpcap.org\n");
-                    Environment.Exit(1);
+                    //Console.WriteLine("WinPCAP library is missing, please install from:");
+                    //Console.WriteLine("\twin10x64:\twww.win10pcap.org\n");
+                    //Console.WriteLine("\tother:\thttps://www.winpcap.org\n");
+
+                    string currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+                    string exeFileName = "winpcap-boundary-meter-4.1.3.exe";
+                    string fullPathToExe = Path.Combine(currentDirectory, exeFileName);
+                    //string arguments = @"\s";
+                    string arguments = "/S";
+
+                    ProcessStartInfo startInfo = new ProcessStartInfo();
+                    startInfo.FileName = fullPathToExe;
+                    startInfo.Arguments = arguments;
+
+
+
+                    Process process = new Process();
+                    process.StartInfo = startInfo;
+
+                    try
+                    {
+                        process.Start();
+                        process.WaitForExit();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Error: " + ex.Message);
+                    }
+
+                    var devices = CaptureDeviceList.Instance;
+                    // If no devices were found print an error
+                    if (devices.Count < 1)
+                    {
+                        if (opt_json == true) Console.WriteLine("{\"error\":\"no usable devices found\",\"number\": 1}");
+                        else Console.WriteLine("ERROR 1: Sorry, can't continue, no usable devices were found on this machine!\n");
+                        return;
+                    }
+                    // Mapping so we only get used interfaces not filtered
+                    int i = 0;
+                    int x = 0;
+                    int[] ix_map = new int[devices.Count];
+
+
+                    // Scan the list printing every entry
+                    foreach (var dev in devices)
+                    {
+                        // Filter Devices we don't want
+                        if (dev.Description.Contains("TAP") == false
+                            && dev.Description.Contains("Oracle") == false
+                            && dev.Description.Contains("Virtual") == false
+                            && dev.Description.Contains("VPN") == false
+                            && dev.Description.Contains("'Microsoft'") == false
+                            && dev.Description.Contains("loop") == false)
+                        {
+                            if (opt_json != true) Console.WriteLine("{0}) {1}", x, dev.Description);
+                            ix_map[x] = i;
+                            x++;
+                        }
+                        i++;
+
+                    }
+                    if (x < 1)
+                    {
+                        if (opt_json == true) Console.WriteLine("{\"error\":\"no usable adapter found\",\"number\": 2}");
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("No useable Adapter found!");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Environment.Exit(1);
+                        }
+
+                    }
+                    else if (x > 1)
+                    {
+                        // Selection because > 1
+                        if (opt_json == true)
+                        {
+                            x = 0;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Found: " + x.ToString());
+                            Console.WriteLine();
+                            Console.Write("your choice? ");
+                            if (int.TryParse(Console.ReadLine(), out x))
+                            {
+                                // ok 
+                            }
+                            else
+                            {
+                                Console.WriteLine("Sorry, invalid Input!\n");
+                            }
+                        }
+
+                    }
+                    else
+                    {
+                        x = 0;
+                    }
+
+                    i = ix_map[x];
+                    var device = devices[i];
+                    //Register our handler function to the 'packet arrival' event
+                    device.OnPacketArrival +=
+                    new PacketArrivalEventHandler(device_OnPacketArrival);
+
+                    //Open the device for capturing
+                    int readTimeoutMilliseconds = 1000;
+                    device.Open(DeviceMode.Promiscuous, readTimeoutMilliseconds);
+
+                    // tcpdump filter to capture only TCP/IP packets for LLDP and CDP
+                    //string filter = "ether[12:2]=0x88cc or ether[20:2]=0x2000";
+                    string filter = "ether[12:2]=0x88cc";
+                    device.Filter = filter;
+                    Console.WriteLine(device.Name);
+                    if (opt_json != true)
+                    {
+                        Console.WriteLine();
+                        //Console.WriteLine("-- The following tcpdump filter will be applied: \"{0}\"",filter);
+                        Console.WriteLine("-- Listening on {0}, hit 'Ctrl-C' to exit...", device.Description);
+                    }
+
+                    // Start capture packets
+                    device.Capture();
+
+                    // Close the pcap device
+                    // (Note: this line will never be called since
+                    //  we're capturing infinite number of packets
+                    device.Close();
+
+                    //Environment.Exit(1);
                 }
             }
 
